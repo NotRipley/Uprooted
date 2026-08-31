9e7108d3296d87ca09f42200452116cc425b95ab
Fergus <68643229+NotRipley@users.noreply.github.com>
2026-08-31 21:47:36 +0100

    bound deck loader to deck class. working on draw method.

--- src/deck.py
+++ src/deck.py
@@ -3,6 +3,7 @@ We model the deck as a list of cards.
 Each card has a suit, cost, vp reward, item reward and description.
 """
 import json
+import numpy as np
 from errors import InvalidCardError
 
 # --- classes ---
@@ -25,9 +26,6 @@ class Card:
         self.vp = vp
         self.item = item
         self.desc = desc
-        # idx -1 to represent not set yet
-        ## What do you mean by the comment above?
-        self.idx = -1 
 
     def bouncer(self, suit, cost, vp, item, desc):
         """Input validation."""
@@ -35,31 +33,53 @@ class Card:
         suits = ["bird", "fox", "mouse", "rabbit"]
         if suit not in suits:
             raise InvalidCardError(f"Expected suit to be {", ".join(suits)} but got {suit}")
-            
-        if not (
-            isinstance(suit, str) and
-            isinstance(cost, str) and
-            isinstance(vp, int) and
-            isinstance(item, str) and
-            isinstance(desc, str)
-        ):
-            raise TypeError(
-                f"Expected (str, str, int, str, str), got"
-                f"({type(suit)}, {type(cost)}, {type(vp)}, {type(item)}, {type(desc)}."
-            )
+
+
 
 class Deck:
-    def __init__(self, cards_list):
-        self.cards = cards_list
+    def __init__(self, filepath):
+        self.load(filepath)
         self.bouncer()
 
     def load(self, filepath):
         "Load the deck from a json object"
-        pass
+        # --- open json and read out inputs ---
+        with open(filepath, "r") as f:
+            deck_config = json.load(f)
+
+        # --- validate json ---
+
+        # --- make list of card objects ---
+        cards_list = []
+        cards_config = deck_config["cards"]
+        for i, card in enumerate(cards_config):
+            item = Card(**card)
+            cards_list.append(item)
+
+        # --- attach card list ---
+        self.cards = cards_list
+        self.draw_pile = self.cards
+        self.discard_pile = []
     
     def draw(self, n):
         "Draw n cards from the deck. Remove the cards drawn from the deck and return them (to be added to hand etc.)"
-        pass
+        # --- check enough cards to draw and if not, reshuffle ---
+        # a point here: it should draw as many as it can, then reshuffle when it hits zero
+        if n > len(self.draw_pile):
+
+            print("not enough cards to draw => reshuffling \n")
+            self.reshuffle()
+
+        # --- draw and return ---
+        draw_indices = np.random.randint(len(self.draw_pile), size=n)
+        cards_to_draw = self.draw_pile[draw_indices]
+        self.draw_pile.pop(draw_indices)
+        return cards_to_draw
+
+    def reshuffle(self):
+        """move discard pile to draw pile"""
+        self.draw_pile.extend(self.discard_pile)
+        self.discard_pile = []
 
     def bouncer(self):
         """Check the cards have been inherited correctly."""
@@ -69,26 +89,7 @@ class Deck:
 
 # --- unbound functions ---
 
-def deck_reader(deck_filepath): # This should be sm like Deck.load(filepath) to initialise the deck
-    """
-    Read in deck JSON and return a Deck object with its cards.
-    :param deck_filepath: filepath of a .json file containing the information for an entire deck of cards.
-    :return: a Deck object.
-    """
-    # --- open json and read out inputs ---
-    with open(deck_filepath, "r") as f:
-        deck_config = json.load(f)
 
-    # --- validate json ---
 
-    # --- make list of card objects ---
-    cards_list = []
-    cards_config = deck_config["cards"]
-    for i, card in enumerate(cards_config):
-        item = Card(**card)
-        item.idx = i # what is this idx for? doesn't seem to be used anywhere?
-        cards_list.append(item)
 
-    # --- give list to Deck instance ---
-    return Deck(cards_list)
 
